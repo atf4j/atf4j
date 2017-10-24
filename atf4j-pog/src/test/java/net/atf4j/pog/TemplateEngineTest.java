c891bd7f8065aaf1ead4adbae52c31d08c0b5425
Martin Spamer <github@atf4j.com>
2017-10-24 10:53:06 +0100

    Fix template engine.

---
+++ atf4j-pog/src/test/java/net/atf4j/pog/TemplateEngineTest.java
@@ -0,0 +1,39 @@
+/**
+ * This file is part of Automated Testing Framework for Java (atf4j).
+ *
+ * Atf4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Atf4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with atf4j.  If not, see http://www.gnu.org/licenses/.
+ */
+
+package net.atf4j.pog;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import net.atf4j.core.TestResultsReporting;
+
+public class TemplateEngineTest extends TestResultsReporting {
+
+    public class TemplateEngine extends AbstractTemplateEngine {
+    }
+
+    @Test
+    public void testExecue() {
+        final TemplateEngine templateEngine = new TemplateEngine();
+        log.debug(templateEngine.toString());
+        assertNotNull(UNEXPECTED_NULL, templateEngine);
+        assertNotNull(templateEngine.execue());
+    }
+
+}
