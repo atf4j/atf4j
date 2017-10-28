17f58bfaab295c3127aa8cba71a00b2246fe3449
Martin Spamer <github@atf4j.com>
2017-10-28 10:54:50 +0100

    Add Generated Code.

---
+++ atf4j-pog/src/generated/java/net/atf4j/generated/JavaClassTwo.java
@@ -0,0 +1,18 @@
+// from main/resources/templates/Class.vm
+// Generated 2017-10-28 10:54.22
+
+package net.atf4j.generated;
+
+public class JavaClassTwo {
+
+	//  String aString;
+	private String aString;
+	//  Boolean aBoolean;
+	private Boolean aBoolean;
+	//  Object aObject;
+	private Object aObject;
+	public JavaClassTwo() {
+		super();
+	}
+
+}
